322a8b8fa5b435d014234a23bd8a2e66b341905b
Melhem Rahmeh <melhem.rahmehh@gmail.com>
2023-04-02 16:24:45 +0300

    test

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import "../index.css";
 import Header from "../components/About/Header";
-import Card from "../components/About/Card";
+import Card from "../components/About/CardComponent";
 import Footer from "../components/Footer/Footer";
 
 const About = () => {
